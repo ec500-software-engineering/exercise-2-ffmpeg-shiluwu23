70aa8ae4c8904fd71e5f2ffde638f3ad638619f8
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2019-02-26 22:08:05 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='exercise-2-ffmpeg-shiluwu23',
+    version='',
+    description='async ffmpeg exercise',
+    author='Shilu Wu',
+    author_email='shiluwu@bu.edu',
+    url='',
+    packages=[''],
+
+)
