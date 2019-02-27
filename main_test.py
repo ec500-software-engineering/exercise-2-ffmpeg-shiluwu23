ec627cde92992dc29003579499b448cfe98f121f
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2019-02-26 23:05:52 -0500

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -18,15 +18,15 @@ def test_duration():
 
     fnin = './video000.mp4'
     fnout_480 = './video000.mp4_480p.mp4'
-    fnout_720 = './video000.mp4_720p.mp4'
+    #fnout_720 = './video000.mp4_720p.mp4'
 
     #orig_meta = ffprobe_sync(fnin)
-    orig_duration = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',inp])
+    orig_duration = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',fnin])
 
     #meta_480 = ffprobe_sync(fnout_480)
    # meta_720 = ffprobe_sync(fnout_720)
     #duration_720 = float(meta_720['streams'][0]['duration'])
-    duration_480 = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',out])
+    duration_480 = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',fnout_480])
 
     assert orig_duration == approx(duration_480)
     #assert orig_duration == approx(duration_720)
