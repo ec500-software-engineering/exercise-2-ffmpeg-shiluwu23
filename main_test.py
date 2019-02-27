f3aff321f63652e090f8877169a523ac7695c293
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2019-02-26 23:08:07 -0500

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,4 +1,3 @@
-#from probe import ffprobe_sync
 from pytest import approx
 import subprocess
 import json
@@ -18,18 +17,18 @@ def test_duration():
 
     fnin = './video000.mp4'
     fnout_480 = './video000.mp4_480p.mp4'
-    #fnout_720 = './video000.mp4_720p.mp4'
+    fnout_720 = './video000.mp4_720p.mp4'
 
-    #orig_meta = ffprobe_sync(fnin)
-    orig_duration = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',fnin])
+    orig_meta = ffprobe_sync(fnin)
+    orig_duration = float(orig_meta['streams'][0]['duration'])
 
-    #meta_480 = ffprobe_sync(fnout_480)
-   # meta_720 = ffprobe_sync(fnout_720)
-    #duration_720 = float(meta_720['streams'][0]['duration'])
-    duration_480 = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',fnout_480])
+    meta_480 = ffprobe_sync(fnout_480)
+    meta_720 = ffprobe_sync(fnout_720)
+    duration_720 = float(meta_720['streams'][0]['duration'])
+    duration_480 = float(meta_480['streams'][0]['duration'])
 
     assert orig_duration == approx(duration_480)
-    #assert orig_duration == approx(duration_720)
+    assert orig_duration == approx(duration_720)
 
 
 if __name__ == '__main__':
