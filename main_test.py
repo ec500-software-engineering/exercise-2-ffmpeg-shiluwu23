38a626d14416f7db9ba808bbf4689a15fc459612
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2019-02-26 22:47:28 -0500

    Add files via upload

---
+++ main_test.py
@@ -0,0 +1,36 @@
+#from probe import ffprobe_sync
+from pytest import approx
+import subprocess
+import json
+from pathlib import Path
+
+
+def ffprobe_sync(filein: Path) -> dict:
+    """ get media metadata """
+    meta = subprocess.check_output(['ffprobe', '-v', 'warning',
+                                	'-print_format', 'json',
+                                	'-show_streams',
+                                	'-show_format',
+                                	str(filein)], universal_newlines = True)
+    return json.loads(meta)
+
+def test_duration():
+
+    fnin = './video000.mp4'
+    fnout_480 = './video000.mp4_480p.mp4'
+    fnout_720 = './video000.mp4_720p.mp4'
+
+    orig_meta = ffprobe_sync(fnin)
+    orig_duration = float(orig_meta['streams'][0]['duration'])
+
+    meta_480 = ffprobe_sync(fnout_480)
+    meta_720 = ffprobe_sync(fnout_720)
+    duration_720 = float(meta_720['streams'][0]['duration'])
+    duration_480 = float(meta_480['streams'][0]['duration'])
+
+    assert orig_duration == approx(duration_480)
+    assert orig_duration == approx(duration_720)
+
+
+if __name__ == '__main__':
+    test_duration()
