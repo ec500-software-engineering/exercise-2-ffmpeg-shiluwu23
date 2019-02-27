a7421c9a9de211ee63a4062fe1ea460c95c724ea
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2019-02-27 11:23:01 -0500

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,9 +1,11 @@
+#from probe import ffprobe_sync
 from pytest import approx
 import subprocess
 import json
 from pathlib import Path
+import main
 
-
+'''
 def ffprobe_sync(filein: Path) -> dict:
     """ get media metadata """
     meta = subprocess.check_output(['ffprobe', '-v', 'warning',
@@ -13,17 +15,20 @@ def ffprobe_sync(filein: Path) -> dict:
                                 	str(filein)], universal_newlines = True)
     return json.loads(meta)
 
+'''
+
+
 def test_duration():
 
     fnin = './video000.mp4'
     fnout_480 = './video000.mp4_480p.mp4'
     fnout_720 = './video000.mp4_720p.mp4'
 
-    orig_meta = ffprobe_sync(fnin)
+    orig_meta = main.ffprobe_sync(fnin)
     orig_duration = float(orig_meta['streams'][0]['duration'])
 
-    meta_480 = ffprobe_sync(fnout_480)
-    meta_720 = ffprobe_sync(fnout_720)
+    meta_480 = main.ffprobe_sync(fnout_480)
+    meta_720 = main.ffprobe_sync(fnout_720)
     duration_720 = float(meta_720['streams'][0]['duration'])
     duration_480 = float(meta_480['streams'][0]['duration'])
 
